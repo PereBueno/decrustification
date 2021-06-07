82fa78285d9224c47eafd36ddc2e9a9d0957a96c
Pedro Bueno <pere.bueno@gmail.com>
2021-06-07 19:06:55 +0200

    Added dates package and base date class, to start with

---
+++ src/main/java/com/pbueno/learning/decrustification/dates/Date.java
@@ -0,0 +1,13 @@
+package com.pbueno.learning.decrustification.dates;
+
+public class Date {
+    public int day;
+    public int month;
+    public int year;
+
+    public Date(int year, int month, int day){
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+}
---
+++ src/main/java/com/pbueno/learning/decrustification/dates/DateAdding.java
@@ -0,0 +1,7 @@
+package com.pbueno.learning.decrustification.dates;
+
+public class DateAdding {
+    private int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+
+}
