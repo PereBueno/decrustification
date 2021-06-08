7f43f2762c9d68e14372b2b2e7c3209c6ed215cc
Pedro Bueno <pere.bueno@gmail.com>
2021-06-08 18:35:44 +0200

    Date addition exercise

--- src/main/java/com/pbueno/learning/decrustification/dates/Date.java
+++ src/main/java/com/pbueno/learning/decrustification/dates/Date.java
@@ -1,13 +1,36 @@
 package com.pbueno.learning.decrustification.dates;
 
+/**
+ * Using Date.java, create a function addDates(int) that allows adding the specified number of days to given date
+ */
 public class Date {
     public int day;
     public int month;
     public int year;
+    private int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     public Date(int year, int month, int day){
         this.year = year;
         this.month = month;
         this.day = day;
+    }
+
+    public Date addDate(int days) {
+        Date newDate;
+        synchronized (this){
+            newDate = new Date(this.year, this.month, this.day);
+        }
+        newDate.day += days;
+        if (newDate.day > daysInMonth[newDate.month-1]){
+            int daysTilMonthEnd = daysInMonth[newDate.month-1] -(newDate.day - days) + 1;
+            newDate.day = 1;
+            newDate.month++;
+            if (newDate.month == 13){
+                newDate.month = 1;
+                newDate.year++;
+            }
+            newDate = newDate.addDate(days - daysTilMonthEnd);
+        }
+        return newDate;
     }
 }
--- src/main/java/com/pbueno/learning/decrustification/dates/DateAdding.java
+++
@@ -1,7 +0,0 @@
-package com.pbueno.learning.decrustification.dates;
-
-public class DateAdding {
-    private int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-
-}
