ae49b2b845c6ded720a7bd64ec5710158a53e055
Pedro Bueno <pere.bueno@gmail.com>
2021-06-08 18:44:16 +0200

    Created base class for roman to decimal translation

---
+++ src/main/java/com/pbueno/learning/decrustification/roman/RomanNumber.java
@@ -0,0 +1,13 @@
+package com.pbueno.learning.decrustification.roman;
+
+/**
+ * Given a roman number, create a method that writes its decimal equivalent
+ */
+
+public class RomanNumber {
+    String roman;
+
+    public RomanNumber(String value){
+        this.roman = value;
+    }
+}
